975d7bb075beda329eb711ced3f11db8cbd5fc96
yu-ko-ba <yu-ko-ba@outlook.com>
2023-08-15 11:32:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/glancetutorial/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.glancetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.glancetutorial.ui.theme.GlanceTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GlanceTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GlanceTutorialTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/glancetutorial/MyAppWidget.kt
@@ -0,0 +1,49 @@
+package com.example.glancetutorial
+
+import android.content.Context
+import android.net.wifi.hotspot2.pps.HomeSp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.Button
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.action.actionStartActivity
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.Text
+
+class MyAppWidget : GlanceAppWidget() {
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+
+        // In this method, load data needed to render the AppWidget.
+        // Use `withContext` to switch to another thread for long running
+        // operations.
+
+        provideContent {
+            // create your AppWidget here
+            MyContent()
+        }
+    }
+
+    @Composable
+    private  fun MyContent() {
+        Column(
+            modifier = GlanceModifier.fillMaxSize(),
+            verticalAlignment = Alignment.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Where to?", modifier = GlanceModifier.padding(12.dp))
+            Row(horizontalAlignment = Alignment.CenterHorizontally) {
+                Button(text = "Home", onClick = actionStartActivity<MainActivity>())
+                Button(text = "Work", onClick = actionStartActivity<MainActivity>())
+            }
+
+        }
+    }
+}
